f6e16ab466f9dc40a9f8f8351fba342a9001f4b2
“X-Jian” <xjian1996@163.com>
2021-02-27 21:56:19 -0800

    5

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Collector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag == "star")
+        {
+            int socre = Int32.Parse(SceneControlls.score);
+            Destroy(collider.gameObject);
+            socre ++;
+            SceneControlls.ChangeScore(socre);
+            string s = "Score: " + socre.ToString();
+            GameObject.FindGameObjectWithTag("scorebox").GetComponent<Text>().text = s;
+        }
+        else if(collider.tag == "Gate")
+        {
+            if (SceneManager.GetActiveScene().name == "Level6")
+            {
+
+                SceneManager.LoadScene("HomeScreen");
+            }
+            else
+            {
+                SceneControlls.CustomLoadScreen("Level"+(SceneManager.GetActiveScene().buildIndex + 1));
+            }
+        }
+    }
+}
